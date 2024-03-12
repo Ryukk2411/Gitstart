71a112ff438bdb2410ffe24e6a10b640b88cfa39
Ryukk2411 <alokrawat28>
2024-03-12 22:58:34 +0530

    commit

---
+++ querySelector & querySelectorAll Methods.js
@@ -0,0 +1,8 @@
+// Write the code as shown in the video below:
+document.querySelector('#main-heading').style.textAlign ='center';
+document.querySelector('.fruits').style.color ='blue';
+// Write answer to the questions asked below:
+const fruit=document.querySelectorAll('.fruits');
+for(let i=0;i<fruit.length;i++){
+  fruit[i].style.color ='blue';
+}
