d92a2731a19ae494038d37f129d9cf7075b963f1
Ryukk2411 <alokrawat28>
2024-03-13 19:15:14 +0530

    commit.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // Add the Edit Button:
+const form = document.querySelector('form');
+const fruits = document.querySelector('.fruits');
 
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const fruitToAdd = document.getElementById('fruit-to-add');
+    const newLi = document.createElement('li');
+    newLi.innerHTML = fruitToAdd.value + '<button class="delete-btn">x</button>'
+    fruits.appendChild(newLi);
+})
 
+fruits.addEventListener('click', function(event){
+    if(event.target.classList.contains('delete-btn')){
+        const fruitToDelete=event.target.parentElement;
+        fruits.removeChild(fruitToDelete);
+    }
+})
 // Implement the code as in video but with one extra 'Edit' button in 'li'
 
